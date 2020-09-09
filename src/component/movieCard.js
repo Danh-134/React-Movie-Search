a826899fb07dae312c8e469798d0d452eae0357f
Danh-134 <d.huynh.hd13@gmail.com>
2020-09-09 16:49:30 +0200

    Organize repository clearly

--- src/component/movieCard.js
+++ src/component/movieCard.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 export default function MovieCard({movie, history}){
-    const title =
-        movie.title.substring(0, 30) + (movie.title.length > 30 ? '...' : '');
+    const title =movie.title.substring(0, 30) + (movie.title.length > 30 ? '...' : '');
     const description =
-        movie.overview.substring(0, 100) +
-        (movie.overview.length > 100 ? '...' : '');
+        movie.overview.substring(0, 250) +
+        (movie.overview.length > 250 ? '...' : '');
 
     return (
          <div className="card" key={movie.id} >
@@ -14,9 +13,9 @@ export default function MovieCard({movie, history}){
                 alt={movie.title + ' poster'}
                 />
             <div className="card--content">
-            <h3 className="card--title">{movie.title}</h3>
-            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-            <p><small>RATING: {movie.vote_average}</small></p>
+            <h3 className="card--title">{title}</h3>
+            <p><small>Date de sortie: {movie.release_date}</small></p>
+            <p><small>NOTE : {movie.vote_average}</small></p>
             <p className="card--desc">{description}</p>
             </div>
 
---
+++ src/component/searchMovie.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import MovieCard from './movieCard.js';
+import '../style.css';
+export default function SearchMovie(){
+
+      //states input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const searchMovies = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=013b92c0b7edec3ee6b2b16351102306&language=fr-FR&query=${query}&page=1&include_adult=false`;
+
+        try{        
+            const res = await fetch(url);
+            const data = await res.json();
+            setMovies(data.results);
+        }catch(err){
+            setError('Failed to fetch movies');
+            setMovies([]);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div>
+            
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Film :</label>
+                <input className="input" type="text" name="query"
+                    placeholder="i.e. Jurassic Park" value={query} onChange={(e) => setQuery(e.target.value)} />
+                <button className="button" type="submit">Recherche </button>
+            </form>
+
+            {loading && <p className="flash info">Chargement...</p>}
+            {error && <p className="flash error">{error}</p>}
+            {!loading && !error && (
+                <div className="card-list">
+                    {movies &&
+                        movies
+                            .filter((movie) => movie.poster_path)
+                            .map((movie) => (
+                                <MovieCard movie={movie} key={movie.id} />
+                            ))}
+                </div>
+            )}
+        </div>
+    );
+}
